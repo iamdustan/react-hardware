e9ff8cf23d313fbf3c21a35f46eef71e1d454244
Dustan Kasten <dustan.kasten@gmail.com>
2016-03-03 07:08:18 -0500

    analytics

--- docs/html.jsx
+++ docs/html.jsx
@@ -76,6 +76,7 @@ module.exports = React.createClass({
         <body className="landing-page">
           <div id="react-mount" dangerouslySetInnerHTML={{__html: this.props.body}} />
           <script src={link("/bundle.js")}/>
+          <script dangerouslySetInnerHTML={{__html: "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-74678007-1', 'auto');ga('send', 'pageview');"}}></script>
         </body>
       </html>
     );

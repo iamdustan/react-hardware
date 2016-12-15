d9b9dc2f2182e88db3d0a407a2061400494f2c3f
Dustan Kasten <dustan.kasten@gmail.com>
2016-12-14 23:05:22 -0500

    Test file to follow renderer path

---
+++ test.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Renderer from './src/ReactHardwareFiber';
+
+console.log(
+  Renderer.render(
+    <pin
+      pin={9}
+      value={1}
+      mode={'OUTPUT'}
+    />,
+    process.env.REACT_HARDWARE_EXAMPLE_PORT || '/dev/cu.usbmodem1451'
+  )
+);
+

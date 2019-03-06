70b4d5c26c10db2a995320c574c60811d7765633
Dustan Kasten <dustan.kasten@gmail.com>
2019-03-05 21:31:55 -0500

    blindly update all the packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,6 @@ module.exports = {
   plugins: [
     'react',
   ],
-  globals: {
-    ReactComponent: true,
-    React$Component: true,
-    ReactClass: true,
-    React$Element: true,
-  },
   rules: {
     'object-curly-spacing': [2, 'never'],
     'react/jsx-uses-react': 2,

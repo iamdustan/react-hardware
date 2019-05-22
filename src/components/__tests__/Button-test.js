dc69b8fb37f390ad6e89f8303e72bf50c3651b3c
Shawn Hoerner <shoerner+git@shoerner.net>
2019-05-22 19:19:28 -0400

    Moves Button-test beforeEach code to individual tests

--- src/components/__tests__/Button-test.js
+++ src/components/__tests__/Button-test.js
@@ -10,46 +10,49 @@ const props = {
   internalPullup: false,
 };
 
-let component;
-let view;
-
 describe('Standard input', () => {
-  beforeEach(() => {
-    component = TestRenderer.create(<Button {...props} />);
-    view = component.toJSON();
-  });
-
   it('should call onDown when the button is pressed', () => {
+    const component = TestRenderer.create(<Button {...props} />);
+    let view = component.toJSON();
+
     view.props.onRead(1);
     view = component.toJSON();
+
     expect(props.onDown).toBeCalledWith({type: "down", value: 1});
     expect(props.onUp).not.toBeCalled();
   });
 
   it('should call onUp when the button is raised', () => {
+    const component = TestRenderer.create(<Button {...props} />);
+    let view = component.toJSON();
+
     view.props.onRead(0);
     view = component.toJSON();
+
     expect(props.onDown).not.toBeCalled();
     expect(props.onUp).toBeCalledWith({type: "up", value: 0});
   });
 });
 
 describe('Internal Pull-Up Set', () => {
-    beforeEach(() => {
-      component = TestRenderer.create(<Button {...props} internalPullup />);
-      view = component.toJSON();
-    });
-  
     it('should call onDown when the button is pressed', () => {
+      const component = TestRenderer.create(<Button {...props} internalPullup />);
+      let view = component.toJSON();
+
       view.props.onRead(0);
       view = component.toJSON();
+
       expect(props.onDown).toBeCalledWith({type: "down", value: 1});
       expect(props.onUp).not.toBeCalled();
     });
   
     it('should call onUp when the button is raised', () => {
+      const component = TestRenderer.create(<Button {...props} internalPullup />);
+      let view = component.toJSON();
+
       view.props.onRead(1);
       view = component.toJSON();
+
       expect(props.onDown).not.toBeCalled();
       expect(props.onUp).toBeCalledWith({type: "up", value: 0});
     });
